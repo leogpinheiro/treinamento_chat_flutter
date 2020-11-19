02dcb0476884387a8cea5ddf6c11cc96f7cadb67
leogpinheiro <leo.grandi@uol.com.br>
2020-11-19 17:13:30 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: GestureDetector(
+        child: Text('Contador: $count'),
+        onTap: () {
+          setState(() {
+            count++;
+          });
+        },
+      )),
+    );
+  }
+}
